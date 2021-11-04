ad6e0aef4dbcb4b398d7debe9cf7db2cb9bf79f0
Basdbruijne <43334573+Basdbruijne@users.noreply.github.com>
2021-11-03 22:33:19 -0300

    Update universal_wrapper.py

--- universal_wrapper.py
+++ universal_wrapper.py
@@ -8,18 +8,32 @@ class UniversalWrapper():
     def run_cmd(self, command):
         return subprocess.check_output(command, shell=True).decode('ascii').splitlines()
 
+    def __call__(self, *args, **kwargs):
+        command = self.cmd+' '
+        command += self.generate_command(*args, **kwargs)
+        output = self.run_cmd(command)
+        return output
+
+    def generate_command(self ,*args, **kwargs):
+        command = ''
+        for string in args:
+            command += str(string)+' '
+        for key, value in kwargs.items():
+            if key == 'root' and value == True:
+                command = 'sudo '+command
+                continue
+            if len(str(key)) > 1:
+                command += '--'+str(key.replace('_', self.divider))+' '
+            else:
+                command += '-'+str(key)+' '
+            if not value == True:
+                command += str(value)+' '
+        return command
+
     def __getattr__(self, name):
         def _wrapped(*args, **kwargs):
-            output = self.cmd+' '+name.replace('_',self.divider)+' '
-            for string in args:
-                output += str(string)+' '
-            for key, value in kwargs.items():
-                if key == 'root' and value == True:
-                    output = 'sudo '+output
-                    continue
-                output += '--'+str(key.replace('_', self.divider))+' '
-                if not value == True:
-                    output += str(value)+' '
-            output = self.run_cmd(output)
+            command = self.cmd+' '+name.replace('_',self.divider)+' '
+            command += self.generate_command(*args, **kwargs)
+            output = self.run_cmd(command)
             return output
         return _wrapped
