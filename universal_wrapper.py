351a24a05ef1de7cf4755709021c872df8326d86
Basdbruijne <43334573+Basdbruijne@users.noreply.github.com>
2021-11-10 21:15:02 -0400

    Add recursion

--- universal_wrapper.py
+++ universal_wrapper.py
@@ -2,15 +2,19 @@ import subprocess
 
 
 class UniversalWrapper:
-    def __init__(self, cmd, divider=" "):
+    def __init__(self, cmd, divider=" ", class_divider=None):
         self.cmd = cmd
         self.divider = divider
+        if class_divider is None:
+            self.class_divider = divider
+        else:
+            self.class_divider = class_divider
 
     def run_cmd(self, command):
+        print(command)
+        print("--")
         command = self.input_modifier(command)
-        output = (
-            subprocess.check_output(command, shell=True).decode("ascii").splitlines()
-        )
+        output = subprocess.check_output(command, shell=True)
         return self.output_modifier(output)
 
     def __call__(self, *args, **kwargs):
@@ -21,7 +25,7 @@ class UniversalWrapper:
         return command
 
     def output_modifier(self, output):
-        return output
+        return output.decode("ascii").splitlines()
 
     def generate_command(self, *args, **kwargs):
         command = ""
@@ -39,9 +43,10 @@ class UniversalWrapper:
                 command += str(value) + " "
         return command
 
-    def __getattr__(self, name):
-        def _wrapped(*args, **kwargs):
-            command = self.cmd + " " + name.replace("_", self.divider) + " "
-            command += self.generate_command(*args, **kwargs)
-            return self.run_cmd(command)
-        return _wrapped
+    def __getattr__(self, attr):
+        subclass = UniversalWrapper(
+            self.cmd + self.class_divider + attr.replace("_", self.divider),
+            self.divider,
+            self.class_divider,
+        )
+        return subclass
